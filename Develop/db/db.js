3d5398c2de0784bd7a99d49de9413efec2cb492d
Joel <guevarra_joel@yahoo.com>
2020-03-30 20:35:28 -0700

    update to files

--- Develop/db/db.js
+++ Develop/db/db.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var utility = require('util');
-var unuiqId = require('uuid/v1');
+var uuidv1 = require('uuid/v1');
 var readFile = utility.promisify(fs.readFile);
 var writeFile = utility.promisify(fs.writeFile);
+
 
 class DB{
     read(){
@@ -13,11 +14,41 @@ class DB{
     }
     getNotes () {
         return this.read().then(notes => {
-            let parsedNotes:
+            let parsedNotes;
             try { parsedNotes =[].concat(JSON.parse(notes)) 
 
             }
+            catch (err){
+                parsedNotes = []
+
+            }
+            return parsedNotes
         })
 
     }
+    add(note){
+        var title = note.title;
+        var text = note.text;
+        if (!title||!text) {
+            throw  new Error ("please enter a title and descritpion");
+        }
+
+        var newnote = {
+
+            title,
+            text,
+            id: uuidv1 ()
+        }
+        return this.getNotes().then(function(allNotes){
+            [...allNotes,newnote]
+            
+        })
+        .then(function(combinedNotes){
+            this.write(combinedNotes).then(()=>newnote)
+            console.log(newnote)
+    
+        })
+       
+    }
 }
+module.exports = new DB()
